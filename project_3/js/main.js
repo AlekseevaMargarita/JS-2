2be95d8b21db2777851de2d9713bd821aac536c0
AlekseevaMargarita <amv1102@mail.ru>
2022-03-09 01:37:54 +0300

    Выполнено задание 1

--- project_3/js/main.js
+++ project_3/js/main.js
@@ -1,8 +1,16 @@
+/* 
+1. Переделайте getRequest() так, чтобы она использовала промисы.
+2. Добавьте в соответствующие классы методы добавления товара в корзину, 
+удаления товара из корзины и получения списка товаров корзины.
+3. Переделайте GoodsList так, чтобы fetchGoods() возвращал промис, 
+а render() вызывался в обработчике этого промиса (сделано на занятии)
+ */
+
 'use strict';
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 // только для первого задания! Далее не использовать в классах каталога и корзины!!!
 // Не использовать fetch
-let getRequest = (url, cb) => {
+/* let getRequest = (url, cb) => {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
 
@@ -17,44 +25,72 @@ let getRequest = (url, cb) => {
   };
 
   xhr.send();
+}; */
+
+const getRequest = (url) => {
+
+  return new Promise((resolve, reject) => {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject(new Error("Ошибка"));
+        } else {
+          console.log(xhr.response)
+          resolve(xhr.response);
+        }
+      }
+    };
+
+    xhr.send();
+
+  });
+
 };
+
 
 class ProductList {
   constructor(container = '.products') {
     this._container = document.querySelector(container);
     this._goods = [];
     this._allProducts = [];
+    /* 
+        this.getProducts()
+          .then((data) => {
+            this._goods = data;
+            this._render();
+          }); */
 
-    this.getProducts()
-        .then((data) => {
-          this._goods = data;
-          this._render();
-        });
+    this._fetchGoods()
 
-    // this._fetchGoods(); // DONT TOUCH PLZ
-    // this._render();
   }
 
-  // _fetchGoods() {
-  //   console.log('fetching data...')
-  //   getRequest(`${API}/catalogData.json`, (data) => {
-  //     console.log('request complete');
-  //     // console.log(data);
-  //     this._goods = JSON.parse(data);
-  //     this._render();
-  //     console.log(this._goods);
-  //   });
-  // }
+  _fetchGoods() {
+    console.log('fetching data...');
+    getRequest(`${API}/catalogData.json`)
+      .then((data) => {
+        this._goods = data;
+        console.log(data);
+        this._render();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   sum() {
     return this._allProducts.reduce((sum, { price }) => sum + price, 0);
   }
 
-  getProducts() {
-   return fetch(`${API}/catalogData.json`)
-       .then((response) => response.json())
-       .catch((err) => console.log(err));
-  }
+  /*   getProducts() {
+      return fetch(`${ API } / catalogData.json`)
+        .then((response) => response.json())
+        .catch((err) => console.log(err));
+    } */
 
   _render() {
     console.log('rendering data...');
@@ -69,21 +105,21 @@ class ProductList {
 
 class ProductItem {
   constructor(product, img = 'https://via.placeholder.com/200x150') {
-    this.id = product.id;
-    this.title = product.title;
+    this.id = product.id_product; //исправлено
+    this.title = product.product_name; //исправлено
     this.price = product.price;
     this.img = img;
   }
 
   getHTMLString() {
-    return `<div class="product-item" data-id="${this.id}">
-              <img src="${this.img}" alt="Some img">
-              <div class="desc">
-                  <h3>${this.title}</h3>
-                  <p>${this.price} \u20bd</p>
-                  <button class="buy-btn">Купить</button>
-              </div>
-          </div>`;
+    return `<div class= "product-item" data - id="${this.id}" >
+      <img src="${this.img}" alt="Some img">
+        <div class="desc">
+          <h3>${this.title}</h3>
+          <p>${this.price} \u20bd</p>
+          <button class="buy-btn">Купить</button>
+        </div>
+      </div>`;
   }
 }
 
